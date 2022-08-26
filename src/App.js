78d783cc300ce684071e4fa1a6d6ae5b6b8b0daf
felipeazs <felipeazs@gmail.com>
2022-08-25 21:13:45 -0400

    Footer, Home, About and NotFound pages and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import NotFound from './components/pages/NotFound';
 
 function App() {
 	return (
 		<Router>
-			<div className='flex flex-col justify-between'>
+			<div className='flex flex-col justify-between h-screen'>
 				<NavBar />
-
-				<main>Content</main>
+				<main className='container mx-auto px-13 pb-12'>
+					<Routes>
+						<Route
+							path='/'
+							element={<HomePage />}
+						/>
+						<Route
+							path='/about'
+							element={<AboutPage />}
+						/>
+						<Route
+							path='*'
+							element={<NotFound />}
+						/>
+					</Routes>
+				</main>
+				<Footer />
 			</div>
 		</Router>
 	);
